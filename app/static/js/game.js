b102f867e68fa2fb63fadf1d0eb1a1a0956d0cdf
Arthur Brenaut <abrenaut@rmn.com>
2016-08-17 20:11:11 +0200

    Load tiles over https

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -45,7 +45,7 @@ function createMap() {
     // Create a world map
     var map = L.map('map', {
         layers: [
-            L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.{ext}', {
+            L.tileLayer('//stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.{ext}', {
                 attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
                 noWrap: true,
                 ext: 'png'
