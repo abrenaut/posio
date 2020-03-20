b1f5c50d6560d1cc329630ab82ed25ec28bf78af
Julien Maulny <julien.maulny@protonmail.com>
2020-03-20 14:03:46 +0100

    Remove useless override config

--- docker/python/config.py
+++ docker/python/config.py
@@ -1,27 +1,3 @@
-# How long is given to players to answer a question
-MAX_RESPONSE_TIME = 5
-
-# Number of seconds between each turns
-TIME_BETWEEN_TURNS = 3
-
-# Number of answer used to compute ranking
-LEADERBOARD_ANSWER_COUNT = 10
-
-# The distance above which score is zero
-SCORE_MAX_DISTANCE = 2000
-
-# Allow players to answer multiple times to the same question
-ALLOW_MULTIPLE_ANSWER = 1  # 0 for False, 1 for True
-
-# How many zoom level are allowed (max 2)
-ZOOM_LEVEL = 0
-
-# CDN URL
-CDN_URL = 'static'
-
-# List of origins that are allowed to connect to this server.
-CORS_ALLOWED_ORIGINS = '*'
-
 # Host and port the server should listen.
 HOST='0.0.0.0'
 PORT=5000
